cd5759989443fe21c54026229cfcba8bcf19c990
Elechi Paschal <140855526+kiddo9@users.noreply.github.com>
2024-10-13 08:35:00 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["index.html",
-          "./src/**/*.{html,js}"],
+          "./src/**/*.{html,js}",
+        './public/index.html'],
   theme: {
     extend: {},
   },
